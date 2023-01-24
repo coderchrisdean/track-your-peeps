cd11c42958179dcb765f768c3cac42226ae4f4cc
coderchrisdean <coderchrisdean@gmail.com>
2023-01-24 12:50:21 -0800

    changed some sql syntax

--- db/sql/schema.sql
+++ db/sql/schema.sql
@@ -1,22 +1,25 @@
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 USE employee_db;  
+DROP TABLE IF EXISTS department;
 CREATE TABLE department (
     id INT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
+    name VARCHAR(30) NOT NULL
 );
 
+
+DROP TABLE IF EXISTS role;
 CREATE TABLE role (
     id INT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL(10,2),
-    department_id INT,
+    department_id INT
 );
-
+DROP TABLE IF EXISTS employee;
 CREATE TABLE employee (
     id INT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT,
+    manager_id INT
 );
--- db/sql/seeds.sql
+++ db/sql/seeds.sql
@@ -7,4 +7,25 @@ VALUES
     (2, 'Engineering'),
     (3, 'Finance'),
     (4, 'Legal');
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES
+    (1, 'Sales Lead', 100000, 1),
+    (2, 'Salesperson', 80000, 1),
+    (3, 'Lead Engineer', 120000, 2),
+    (4, 'Software Engineer', 100000, 2),
+    (5, 'Accountant', 130000, 3),
+    (6, 'Legal Team Lead', 130000, 4),
+    (7, 'Lawyer', 120000, 4);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES
+    (800283001, "Christopher", "Dean", 1, 1),
+    (800283002, "John", "Smith", 2, 1),
+    (800283003, "Jane", "Doe", 3, 1),
+    (800283004, "Mary", "Jane", 4, 1),
+    (800283005, "Sally", "May", 5, 1),
+    (800283006, "Bob", "Jones", 6, 1),
+    (800283007, "Sue", "Smith", 7, 1);
     
+     
