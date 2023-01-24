8ee06a042f9389d7a5d5714c5c0bbfa49faab02b
coderchrisdean <coderchrisdean@gmail.com>
2023-01-24 12:03:45 -0800

    coded schema and seeds, added queries.sql file

---
+++ db/sql/seeds.sql
@@ -0,0 +1,10 @@
+-- Seed The Schema With Intial Information
+-- --------------------------------------
+
+INSERT INTO department (id, name)
+VALUES
+    (1, 'Sales'),
+    (2, 'Engineering'),
+    (3, 'Finance'),
+    (4, 'Legal');
+    
