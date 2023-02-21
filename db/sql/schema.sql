3205c585f7050102b9f14677e0ab9881f6b089ef
coderchrisdean <coderchrisdean@gmail.com>
2023-02-21 15:19:12 -0800

    fixed syntax errors in schema.sql

--- db/sql/schema.sql
+++ db/sql/schema.sql
@@ -1,23 +1,24 @@
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 USE employee_db;  
+
 DROP TABLE IF EXISTS department;
 CREATE TABLE department (
-    id INT PRIMARY KEY AUTO INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL
 );
 
-
 DROP TABLE IF EXISTS role;
 CREATE TABLE role (
-    id INT PRIMARY KEY AUTO INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30),
     salary DECIMAL(10,2),
     department_id INT
 );
+
 DROP TABLE IF EXISTS employee;
 CREATE TABLE employee (
-    id INT PRIMARY KEY AUTO INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
